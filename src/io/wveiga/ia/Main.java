002ff7c5869c81c08e09c6363b5ee9133d7b628d
Welington Veiga <welington.veiga@gmail.com>
2017-03-05 21:04:38 -0300

    Ajustes no código de testes.

--- src/io/wveiga/ia/Main.java
+++ src/io/wveiga/ia/Main.java
@@ -3,8 +3,6 @@ package io.wveiga.ia;
 import java.util.List;
 import java.util.Optional;
 
-import io.wveiga.ia.algs.busca.naoinfo.BuscaLargura;
-import io.wveiga.ia.algs.busca.naoinfo.BuscaNaoInformada;
 import io.wveiga.ia.algs.busca.naoinfo.BuscaProfundidade;
 import io.wveiga.ia.problema.Acao;
 import io.wveiga.ia.problema.npuzzle.NPuzzle;
@@ -15,39 +13,25 @@ public class Main {
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		
-		/*
-		 |2 | 3 | , 
-		 |- | 1 |
-		// NPuzzleTabuleiro tabuleiro = new NPuzzleTabuleiro(2, new int[][]{{2, 3}, {0, 1}});
+		// 1 - Criando Problema que deve ser resolvido
 		NPuzzleTabuleiro tabuleiro = new NPuzzleTabuleiro(3);
 		NPuzzle puzzle = new NPuzzle(tabuleiro);
+		
+		// 2 - Instânciando Busca.
 		BuscaProfundidade<NPuzzleTabuleiro, NPuzzle> busca = new BuscaProfundidade<>(puzzle);
+		
+		// 3 - Obtendo passos para solução ou EMPTY caso a busca não tenha encontrado solução.
 		Optional<List<Acao<NPuzzleTabuleiro>>> acoes = busca.buscaIterativa();
 		if (acoes.isPresent()){
+			// 3.1 Caso a solução tenha sido encontrada, imprimir passos.
 			for (Acao<NPuzzleTabuleiro> acao : acoes.get()) {
-				System.  out.println(acao.getEstadoAlvo());
-			}
-		}
-		
-		long end = System.currentTimeMillis();
-		
-		System.out.println("Tempo total: "+ (end - start));*/
-		
-		NPuzzleTabuleiro tabuleiro = new NPuzzleTabuleiro(3);
-		NPuzzle puzzle = new NPuzzle(tabuleiro);
-		BuscaNaoInformada<NPuzzleTabuleiro, NPuzzle> busca = new BuscaLargura<>(puzzle);
-		Optional<List<Acao<NPuzzleTabuleiro>>> acoes = busca.buscar();
-		if (acoes.isPresent()){
-			for (Acao<NPuzzleTabuleiro> acao : acoes.get()) {
-				System.  out.println(acao.getEstadoAlvo());
+				System.out.println(acao.getEstadoAlvo());
 			}
 		}
 		
 		long end = System.currentTimeMillis();
 		
 		System.out.println("Tempo total: "+ (end - start));
-		
-
 	}
 
 }
